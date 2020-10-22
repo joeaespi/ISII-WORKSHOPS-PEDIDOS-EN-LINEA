86a49109293daf7f0426f13fc2c8642da3bd0a1d
joeaespi <joel_es_@hotmail.com>
2020-10-22 18:28:37 -0500

    Premium calculation developer

---
+++ ec.edu.espol.workshops.second/src/test/java/ec/edu/espol/workshops/second/Customer.java
@@ -0,0 +1,20 @@
+package ec.edu.espol.workshops.second;
+
+public class Customer {
+	private int age;
+	private String sex;
+	private String state;
+	private int prima ;
+	
+	public Customer(int age, String sex, String state) {
+		super();
+		this.age = age;
+		this.sex = sex;
+		this.state = state;
+		this.prima = 500;
+	}
+
+	
+	
+	
+}
