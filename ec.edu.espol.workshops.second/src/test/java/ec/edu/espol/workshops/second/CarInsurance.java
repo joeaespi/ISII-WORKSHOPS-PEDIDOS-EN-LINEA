6653429ae10faa25273a71cba4abae630c22882b
joeaespi <joel_es_@hotmail.com>
2020-10-29 19:31:33 -0500

    Cambios con respecto con el uso de CheckStyle

--- ec.edu.espol.workshops.second/src/test/java/ec/edu/espol/workshops/second/CarInsurance.java
+++ ec.edu.espol.workshops.second/src/test/java/ec/edu/espol/workshops/second/CarInsurance.java
@@ -1,40 +1,41 @@
 package ec.edu.espol.workshops.second;
-import java.util.Scanner;
+
+import  java.util.Scanner;
 
 public class CarInsurance {
-	public static void main(String[] args) {
-		int prima =500;
-		Scanner entrada = new Scanner(System.in);
-		
-		System.out.println("The insurance company");
-		
-		System.out.print("Su sexo: M/F");
-		String sexo = entrada.nextLine();
-		System.out.print("Estad civil: married/not married");
-		String state = entrada.nextLine();
-		System.out.print("Ingrese su edad");
-		int edad = entrada.nextInt();
-		
-		Customer customer = new Customer(edad,sexo,state);
+  /**Aquí se realizan las pruebas.
 
-		if(customer.getSex().equals('M')&& customer.getAge() < 25 && customer.getState().equalsIgnoreCase("not married")) {
-			customer.setPrima(customer.getPrima()+1500);
-			System.out.print(customer.getPrima());
-		}
-		if(sexo.equals('M') || state.equalsIgnoreCase("married") ) {
-			customer.setPrima(customer.getPrima()-200);
-			System.out.print(customer.getPrima());
-		}
-		
-		if(edad > 45 && edad<65) {
-			customer.setPrima(customer.getPrima()-100);
-			prima-=100;
-			System.out.print(customer.getPrima());
-		}
-		
-		
-	}
-	
-	
+   * @author: Mario R. Rancel
 
-}
+   * @version: 22/09/2016/A
+
+   * @see a
+
+   */
+  public static void main(String[] args) {
+    int prima = 500;
+    Scanner entrada = new Scanner(System.in);
+    System.out.println("The insurance company");
+    System.out.print("Su sexo: M/F");
+    String sexo = entrada.nextLine();
+    System.out.print("Estad civil: married/not married");
+    String state = entrada.nextLine();
+    System.out.print("Ingrese su edad");
+    int edad = entrada.nextInt();
+    Customer customer = new Customer(edad, sexo, state);
+    if (customer.getSex().equals('M') && customer.getAge() < 25 
+        && customer.getState().equalsIgnoreCase("not married")) {
+      customer.setPrima(customer.getPrima() + 1500);
+      System.out.print(customer.getPrima());
+    }
+    if (sexo.equals('M') || state.equalsIgnoreCase("married")) {
+      customer.setPrima(customer.getPrima() - 200);
+      System.out.print(customer.getPrima());
+    }
+    if (edad > 45 && edad < 65) {
+      customer.setPrima(customer.getPrima() - 100);
+      prima -= 100;
+      System.out.print(customer.getPrima());
+    }
+  }
+} //terminación de la clase
--- ec.edu.espol.workshops.second/src/test/java/ec/edu/espol/workshops/second/Customer.java
+++ ec.edu.espol.workshops.second/src/test/java/ec/edu/espol/workshops/second/Customer.java
@@ -1,47 +1,54 @@
 package ec.edu.espol.workshops.second;
 
 public class Customer {
-	private int age;
-	private String sex;
-	private String state;
-	private int prima ;
-	
-	public Customer(int age, String sex, String state) {
-		this.age = age;
-		this.sex = sex;
-		this.state = state;
-		this.prima = 500;
-	}
+  private int age;
+  private String sex;
+  private String state;
+  private int prima;
 
-	public int getAge() {
-		return this.age;
-	}
-	
-	public String getSex() {
-		return this.sex;
-	}
-	
-	public String getState() {
-		return this.state;
-	}
-	
-	public int getPrima() {
-		return this.prima;
-	}
-	
-	public void setAge(int age) {
-		this.age=age;
-	}
-	
-	public void setSex(String sex) {
-		this.sex=sex;
-	}
-	
-	public void setState(String state) {
-		this.state=state;
-	}
-	
-	public void setPrima(int prima) {
-		this.prima=prima;
-	}
+  /**
+   * Esta clase es customer.
+   * @author: Joel
+   * @author: Joel
+   * @version: 1
+   * @see CarInsurance
+   */
+  public Customer(int age, String sex, String state) {
+    this.age = age;
+    this.sex = sex;
+    this.state = state;
+    this.prima = 500;
+  }
+
+  public int getAge() {
+    return this.age;
+  }
+
+  public String getSex() {
+    return this.sex;
+  }
+
+  public String getState() {
+    return this.state;
+  }
+
+  public int getPrima() {
+    return this.prima;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public void setSex(String sex) {
+    this.sex = sex;
+  }
+
+  public void setState(String state) {
+    this.state = state;
+  }
+
+  public void setPrima(int prima) {
+    this.prima = prima;
+  }
 }
