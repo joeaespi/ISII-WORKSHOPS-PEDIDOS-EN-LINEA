e0321ff4155ef91246f714799969592e8d0ceb1c
Dario Triviño <darojos1997@gmail.com>
2020-11-08 16:54:27 -0500

    Merge completo

--- ec.edu.espol.workshops.second/src/test/java/ec/edu/espol/workshops/second/CarInsurance.java
+++ ec.edu.espol.workshops.second/src/test/java/ec/edu/espol/workshops/second/CarInsurance.java
@@ -1,5 +1,6 @@
 package ec.edu.espol.workshops.second;
 import  java.util.Scanner;
+import java.util.logging.Logger;
 
 public class CarInsurance {
   /**Aquí se realizan las pruebas.
@@ -11,30 +12,31 @@ public class CarInsurance {
    * @see a
 
    */
-  public static void main(String[] args) {
-    int prima = 500;
-    Scanner entrada = new Scanner(System.in);
-    System.out.println("The insurance company");
-    System.out.print("Su sexo: M/F");
-    String sexo = entrada.nextLine();
-    System.out.print("Estad civil: married/not married");
-    String state = entrada.nextLine();
-    System.out.print("Ingrese su edad");
-    int edad = entrada.nextInt();
-    Customer customer = new Customer(edad, sexo, state);
-    if (customer.getSex().equals('M') && customer.getAge() < 25 
-        && customer.getState().equalsIgnoreCase("not married")) {
-      customer.setPrima(customer.getPrima() + 1500);
-      System.out.print(customer.getPrima());
-    }
-    if (sexo.equals('M') || state.equalsIgnoreCase("married")) {
-      customer.setPrima(customer.getPrima() - 200);
-      System.out.print(customer.getPrima());
-    }
-    if (edad > 45 && edad < 65) {
-      customer.setPrima(customer.getPrima() - 100);
-      prima -= 100;
-      System.out.print(customer.getPrima());
-    }
-  }
+	private static final Logger LOG = Logger.getLogger("CarInsurance");
+	public static void main(String[] args) {
+		int prima = 500;
+		Scanner entrada = new Scanner(System.in);
+		LOG.info("The insurance company");
+		LOG.info("Su sexo: M/F");
+		String sexo = entrada.nextLine();
+		LOG.info("Estad civil: married/not married");
+		String state = entrada.nextLine();
+		LOG.info("Ingrese su edad:");
+		int edad = entrada.nextInt();
+		Customer customer = new Customer(edad, sexo, state);
+		if (customer.getSex().equals('M') && customer.getAge() < 25 
+				&& customer.getState().equalsIgnoreCase("not married")) {
+			customer.setPrima(customer.getPrima() + 1500);
+			LOG.info(String.valueOf(customer.getPrima()));
+		}
+		if (sexo.equals('M') || state.equalsIgnoreCase("married")) {
+			customer.setPrima(customer.getPrima() - 200);
+			LOG.info(String.valueOf(customer.getPrima()));
+		}
+		if (edad > 45 && edad < 65) {
+			customer.setPrima(customer.getPrima() - 100);
+			prima -= 100;
+			LOG.info(String.valueOf(customer.getPrima()));
+		}
+	}
 } //terminación de la clase
